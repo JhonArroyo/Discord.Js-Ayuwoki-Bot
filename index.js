c607ffdab3637f6c7e1d1299eff77ed691ffb5ae
JhonArroyo <jhonsnipertf18@gmail.com>
2021-05-15 10:40:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ return itemx[Math.floor(Math.random()*itemx.length)];
 
 const itemx = [
   "https://i.imgur.com/o9hA330.png",
-  "https://i.imgur.com/VzVHTTG.gif"
+  "https://i.imgur.com/VzVHTTG.gif",
+  "https://img.wattpad.com/cover/81461955-512-k104018.jpg",
+  "https://i.imgur.com/9DhvFAs.jpeg"
 ]; 
 
 
@@ -108,4 +110,4 @@ client.on('message', message => {
 });
 
 
-client.login('ODQyODU5NTg1NDY3NzExNDg4.YJ7cKA.SIkFNgRZGn6Z4Y6umKaTVWE7KTs');
+client.login(' ');
